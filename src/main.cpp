4f1dea62b0a5c21e873c7aa66b936925660b7bf1
Yehor Sorokin <egormonte2015@gmail.com>
2024-11-23 15:48:11 +0100

    Basic Calculations ( +,  -,  /,  * ) with two numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main () {
 
+    // Basic variables
+    string operation;
+    double num1, num2;
+
+    // User enter
+    cout << "Calculate: " << endl;
+    cin >> num1 >> operation >> num2;
+
+    // Result
+    
+    if (operation == "+")
+            
+        cout << "--- Calculator ---" << endl 
+             << "Result: " << num1 + num2 << endl;
+
+    else if (operation == "-")
+            
+        cout << "--- Calculator ---" << endl 
+             << "Result: " << num1 - num2 << endl;
+
+    else if (operation == "/")
+            
+        cout << "--- Calculator ---" << endl 
+             << "Result: " << num1 / num2 << endl ;
+
+    else if (operation == "*")
+            
+        cout << "--- Calculator ---" << endl 
+             << "Result: " << num1 * num2 << endl;
 
     return 0;
 }
